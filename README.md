f31b4c3db063bdd40b6b68b41bd56a183ac9a0f9
Scott Hain <shain@chef.io>
2016-07-26 15:58:42 -0700

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# zone
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'zone'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures zone'
+long_description 'Installs/Configures zone'
+version '0.1.0'
